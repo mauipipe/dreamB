218594f6eab6fd679e74c6289e3ad2b2629a69ae
David Contavalli <mauipipe@gmail.com>
2015-05-28 16:45:26 +0200

    empty api blueprint page added

---
+++ module/API/Module.php
@@ -0,0 +1,30 @@
+<?php
+namespace API;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getControllerConfig()
+    {
+        return array(
+            'invokables' => array(
+                'API\Controller\Index' => 'API\Controller\IndexController'
+            )
+        );
+    }
+}
---
+++ module/API/config/module.config.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Hostname',
+                'options' => array(
+                    'route'    => 'api.dream-beach.local',
+                    'defaults' => array(
+                        'controller' => 'API\Controller\Index'
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+        'strategies' => array(
+            'ViewJsonStrategy',
+        ),
+    )
+);
