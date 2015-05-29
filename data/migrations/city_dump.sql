11703aae4063aab930eb23e46588f716e05f29c8
David Contavalli <mauipipe@gmail.com>
2015-05-29 19:08:18 +0200

    migration with all city added

---
+++ data/migrations/city_dump.sql
@@ -0,0 +1,16 @@
+INSERT INTO dreambeach.city (name, slug)
+VALUES ("Napoli","napoli"),
+("Bari", "bari"),
+("Taranto","taranto"),
+("Reggio Calabria", "reggio-calabria"),
+("Foggia","foggia"),
+("Salerno","salerno"),
+("Pescara", "pescara"),
+("Andria", "adria"),
+("Barletta", "barletta"),
+("Lecce", "lecce"),
+("Catanzaro", "catanzaro"),
+("Brindisi", "brindisi"),
+("Torre del Greco", "torre-del-greco"),
+("Pozzuoli", "pozzuoli"),
+("Casoria", "casoria")
